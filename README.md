75b63a2c8edece0babe29cbf04485b0b4fe827a6
Jaezat <163866069+Jaezat@users.noreply.github.com>
2025-02-10 17:02:36 +0000

    Revert "Aiuda"

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # corrections
 Correcciones de los ejercicios en C
-Ultimo intento haciendo un pull request
+HOLI
 
--- ftputnbr.c
+++ ftputnbr.c
@@ -15,15 +15,16 @@ void put_positive(int p)
 		ft_putchar(p + '0');
 	}
 
-	else if (p >= 10)
-	{	
-		put_positive(p/10);   
-		ft_putchar((p = p % 10) + '0');  
-	}
+	else if (p >= 10) // no lo se rick
+	{
+		ft_putchar((p = p / 10) + '0'); // el tema con esto es que no imprime numero mas de dos digitos
+		ft_putchar((p = p % 10) + '0'); // al ser mas de dos digitos imprime por ejemplo 3235/10= 323 + '0' = que no tiene que ver con un numero en ASCII necesariameente
+	} 
+
 
 }
 
- void put_negative(int n)
+void put_negative(int n)
 {
 	if (n > -10 && n < 0)
 	{
@@ -31,15 +32,13 @@ void put_positive(int p)
 		ft_putchar((n * (-1)) + '0');
 	}
 	
-
-	else if (n <= -10) 
-	{	
-		put_negative(n/10);
-		ft_putchar(((n % 10) * -1) + '0');		 
-		
+	else if (n <= -10) // same here du coup 
+	{
+		ft_putchar('-');
+		ft_putchar(((n / 10) * -1) + '0');
+		ft_putchar(((n % 10) * -1) + '0');
 	}  
 }
-
 
 void ft_putnbr(int nbr)
 {
@@ -58,7 +57,7 @@ void ft_putnbr(int nbr)
 
 int main (void)
 {
-	ft_putnbr(-65382);
+	ft_putnbr(8902);
 
 	return 0;
 } 
