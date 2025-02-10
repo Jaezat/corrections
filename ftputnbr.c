e0ae3f93b8326f707c8391f96d1e78bca6e85763
Jazmin Acosta <jaezatc@gmail.com>
2025-02-10 16:56:13 +0000

    Last modification (I hope)

--- ftputnbr.c
+++ ftputnbr.c
@@ -15,16 +15,15 @@ void put_positive(int p)
 		ft_putchar(p + '0');
 	}
 
-	else if (p >= 10) // no lo se rick
-	{
-		ft_putchar((p = p / 10) + '0'); // el tema con esto es que no imprime numero mas de dos digitos  
-		ft_putchar((p = p % 10) + '0'); // al ser mas de dos digitos imprime por ejemplo 3235/10= 323 + '0' = que no tiene que ver con un numero en ASCII necesariameente
-	} 
-
+	else if (p >= 10)
+	{	
+		put_positive(p/10);   
+		ft_putchar((p = p % 10) + '0');  
+	}
 
 }
 
-void put_negative(int n)
+ void put_negative(int n)
 {
 	if (n > -10 && n < 0)
 	{
@@ -32,13 +31,15 @@ void put_negative(int n)
 		ft_putchar((n * (-1)) + '0');
 	}
 	
-	else if (n <= -10) // same here du coup 
-	{
-		ft_putchar('-');
-		ft_putchar(((n / 10) * -1) + '0');
-		ft_putchar(((n % 10) * -1) + '0');
+
+	else if (n <= -10) 
+	{	
+		put_negative(n/10);
+		ft_putchar(((n % 10) * -1) + '0');		 
+		
 	}  
 }
+
 
 void ft_putnbr(int nbr)
 {
@@ -57,7 +58,7 @@ void ft_putnbr(int nbr)
 
 int main (void)
 {
-	ft_putnbr(65382);
+	ft_putnbr(-65382);
 
 	return 0;
 } 
